85404d36de03b14df55348e81dbdbdd07f462e6c
tiltishche <igor_druchinin@rambler.ru>
2021-02-16 22:40:33 +0300

    a v b

---
+++ FirstWeekThreeTask/FirstWeekThreeTask.cpp
@@ -0,0 +1,19 @@
+﻿
+#include <iostream>
+int main()
+{
+    
+    int a, b;
+
+    std::cout << "Enter number a: ";
+    std::cin >> a;
+    std::cout << "Enter number b: ";
+    std::cin >> b;
+
+    a += b;
+    b = a - b;
+    a -= b;
+    std::cout << "a=" << a << " " << "b=" << b;
+    std::getchar(); 
+    std::getchar(); 
+}

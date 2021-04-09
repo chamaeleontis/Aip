ba2cef39155c85f42659adfef419bc7305103bf0
tiltishche <igor_druchinin@rambler.ru>
2021-04-09 09:27:55 +0300

    3 zadanie ciklbI

---
+++ 7Week/3/3.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <iomanip> // чтобы работал precision
+#include <cmath>
+using namespace std;
+/*
+Протабулировать функцию y=sqrt(5+4x)+13x+cos(12x+91) (шаг и диапазон задаёт пользователь):
+ */
+int main()
+{
+    float sh, x1, x, y;
+    cout << "Vvedite shag: ";
+    cin >> sh;
+    cout << "Vvedite nachalo diapazona: ";
+    cin >> x;
+    cout << "Vvedite konec diapazona: ";
+    cin >> x1;
+    // заголовок таблицы
+    cout << "\t x \t \t y" << endl;
+    cout.precision(5);
+    while (x <= x1) {
+        y = sqrt(5 + 4*x) + 13 * x + cos(12 * x + 91);
+        cout << "\t" << x << "\t\t" << y << endl;
+        x += sh;
+    }
+    return 0;
+}

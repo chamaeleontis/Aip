74f832b2d55c2d5f6f11862d95f0db3b2877a037
tiltishche <igor_druchinin@rambler.ru>
2021-04-09 08:33:41 +0300

    1 zadanie ciklbI

---
+++ 7Week/1/1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+/*
+Вывести на экран Н строк из нулей, причем количество нулей в каждой строке
+равно номеру строки. Количество строк Н вводит пользователь.
+ */
+
+int main() {
+    float N;
+    std::cout << "Vvedite kolichestvo strok:" << std::endl;
+    std::cin >> N;
+    for (int i = 1; i < N + 1; i++) {
+        for (int j = 0; j < i; j++)
+            std::cout << 0;
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
